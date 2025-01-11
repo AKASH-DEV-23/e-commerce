b698a7cc2f49ce6378ff32cba96ac7abe5bb6a14
AKASH KUMAR <akashkumarcs23@gmail.com>
2025-01-12 00:01:53 +0530

    clodinary functionality added

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,5 @@
+import cloudinary from "../lib/cloudinary.js";
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 
@@ -12,3 +14,59 @@ export const getAllProducts = async (req, res) => {
 
     }
 };
+
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        let featuredProducts = await redis.get("featured_products");
+        if (featuredProducts) {
+            return res.json(JSON.parse(featuredProducts));
+        }
+
+        // if not in redis fetch from mongoDB
+        // .lean() is gonna to return a plain javascript object instead of mongoose document
+        // which is good for performance
+        featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+        if (!featuredProducts) {
+            return res.status(404).json({ message: "No featured products found" });
+        }
+
+        // store in redis for future quick access
+
+        await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+        res.json(featuredProducts);
+
+    } catch (error) {
+        console.log("Error in getFeaturedProducts controller", error.message);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+
+    }
+};
+
+
+export const createProduct = async (req, res) => {
+    try {
+        const { name, description, price, image, category } = req.body;
+        let cloudinaryResponse = null;
+
+        if (image) {
+            cloudinaryResponse = await cloudinary.uploader.upload(image, { folder: "products" });
+        }
+
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+            category
+        })
+
+        res.status(201).json(product);
+    } catch (error) {
+        console.log("Error in createProduct controller", error.message);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+
+    }
+}
