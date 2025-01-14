397aef4738a7106120a5715788aba6af9f390e0c
AKASH KUMAR <akashkumarcs23@gmail.com>
2025-01-15 00:46:32 +0530

    add product to cart functionlity added

---
+++ backend/routes/payment.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { createCheckoutSession } from "../controllers/payment.controller.js";
+
+const router = express.Router();
+
+router.post("/create-checkout-session", protectRoute, async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+export default router;
