c0def95a7585521e278954827b7228950adcf603
AKASH KUMAR <akashkumarcs23@gmail.com>
2025-01-18 12:33:14 +0530

    backed fuctionality completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -160,4 +160,12 @@ export const refreshToken = async (req, res) => {
 }
 
 
-export const getProfile = async (req, res) => { };
+export const getProfile = async (req, res) => {
+  try {
+    res.json(req.user);
+
+  } catch (error) {
+    console.log("Error in getProfile controller", error.message);
+    res.status(500).json({ message: "Internal server error", error: error.message });
+  }
+};
--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,15 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { createCheckoutSession } from "../controllers/payment.controller.js";
+import { checkoutSuccess, createCheckoutSession } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
-router.post("/create-checkout-session", protectRoute, async (req, res) => {
-    try {
+router.post("/create-checkout-session", protectRoute, createCheckoutSession)
+router.post("/checkout-success", protectRoute, checkoutSuccess)
 
-    } catch (error) {
-
-    }
-});
 
 export default router;
